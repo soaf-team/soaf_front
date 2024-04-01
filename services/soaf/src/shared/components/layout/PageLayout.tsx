93d80381c70825ac183632464c0611eef0f0bd2a
jang_yoonsu <shagrat8546@gmail.com>
2024-04-01 23:59:11 +0900

    feat : 페이지 레이아웃 구현

---
+++ services/soaf/src/shared/components/layout/PageLayout.tsx
@@ -0,0 +1,23 @@
+import { Header } from "./Header";
+
+type PageLayoutProps = {
+  children: React.ReactNode;
+  header?: {
+    title: React.ReactNode;
+    leftSlot?: React.ReactNode;
+    rightSlot?: React.ReactNode;
+  };
+};
+
+export const PageLayout = ({ children, header }: PageLayoutProps) => {
+  return (
+    <>
+      {header && (
+        <Header leftSlot={header.leftSlot} rightSlot={header.rightSlot}>
+          {header.title}
+        </Header>
+      )}
+      <main className="py-[18px]">{children}</main>
+    </>
+  );
+};
