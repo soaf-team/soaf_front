fd28c836cb61f87d53fdbb58324fa6acdf7e5b85
jang_yoonsu <shagrat8546@gmail.com>
2024-04-01 23:59:58 +0900

    feat : 메인 액티비티 추가 (예시)

--- services/soaf/src/pages/Main.tsx
+++ services/soaf/src/pages/Main.tsx
@@ -1,10 +1,29 @@
 import { ActivityComponentType } from "@stackflow/react";
+import { useFlow } from "@src/stackflow";
+import { PageLayout } from "@src/shared/components";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 
 const Main: ActivityComponentType = () => {
+  const { push } = useFlow();
+
+  const onClick = () => {
+    push("Article", {
+      title: "Hello",
+    });
+  };
+
   return (
-    <AppScreen appBar={{ title: "Main" }}>
-      <div>My Activity</div>
+    <AppScreen>
+      <PageLayout
+        header={{
+          title: "메인",
+        }}
+      >
+        <div>
+          My Activity
+          <button onClick={onClick}>Go to article page</button>
+        </div>
+      </PageLayout>
     </AppScreen>
   );
 };
