7d81425479460e3f9bfe22f59aa9ebe808208eaa
ynnsuis <shagrat@stoa-investment.com>
2024-04-03 21:01:31 +0900

    chore : shadcn 라이브러리 추가

--- services/soaf/tailwind.config.js
+++ services/soaf/tailwind.config.js
@@ -1,8 +1,57 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#57C2FF",
+        black: "#121212",
+        white: "#ffffff",
+        BORDER: "#8A91A833",
+        grey50: "#F0F1F4",
+        grey100: "#E2E3E9",
+        grey200: "#A7ACBD",
+        grey300: "#8A91A8",
+        grey400: "#6D7592",
+        grey500: "#575E75",
+        grey600: "#414658",
+        grey700: "#2C2F3A",
+        grey800: "#2C2F3A",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
