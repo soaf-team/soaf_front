c628d6e9b348a2b26d2ec1ed337d3345cc2c345a
jang_yoonsu <shagrat8546@gmail.com>
2024-04-06 21:12:26 +0900

    style : 스피너 애니메이션 추가

--- services/soaf/tailwind.config.js
+++ services/soaf/tailwind.config.js
@@ -50,8 +50,13 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        spin: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
       },
       animation: {
+        spin: "spin 1s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
