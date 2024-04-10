b1d1b1a62dacd247761ce0f46fc1ae99341ba02b
H0onnn <dukei2017@naver.com>
2024-04-10 19:02:26 +0900

    refactor : emotion icon constant로 분리

---
+++ services/soaf/src/shared/constants/emotion.ts
@@ -0,0 +1,27 @@
+import angry from "@assets/emotions/angry.svg";
+import anxious from "@assets/emotions/anxious.svg";
+import comportable from "@assets/emotions/comportable.svg";
+import flutter from "@assets/emotions/flutter.svg";
+import funny from "@assets/emotions/funny.svg";
+import gloomy from "@assets/emotions/gloomy.svg";
+import happy from "@assets/emotions/happy.svg";
+import lonely from "@assets/emotions/lonely.svg";
+import proud from "@assets/emotions/proud.svg";
+import pleased from "@assets/emotions/pleased.svg";
+import sad from "@assets/emotions/sad.svg";
+import tired from "@assets/emotions/tired.svg";
+
+export const EMOTION_ICON = {
+  행복한: happy,
+  기분좋은: pleased,
+  즐거운: funny,
+  설레는: flutter,
+  뿌듯한: proud,
+  편안한: comportable,
+  피곤한: tired,
+  외로운: lonely,
+  슬픈: sad,
+  우울한: gloomy,
+  불안한: anxious,
+  화난: angry,
+};
