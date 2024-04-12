82982b74b99abc46ea880e9e0d4ecb14c342b1f5
H0onnn <dukei2017@naver.com>
2024-04-13 01:42:04 +0900

    design : emotion sticker css 수정

--- services/soaf/src/shared/components/emotion/EmotionSticker.tsx
+++ services/soaf/src/shared/components/emotion/EmotionSticker.tsx
@@ -7,8 +7,12 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   size?: string;
 }
 
-const EmotionSticker = ({ emotion, size = "md", className, style }: Props) => {
-  const stickerSize = size === "md" ? "w-[42px] h-[42px]" : "w-[16px] h-[16px]";
+const EmotionSticker = ({ emotion, size = "lg", className, style }: Props) => {
+  const stickerSize = {
+    sm: "w-[16px] h-[16px]",
+    md: "w-[32px] h-[32px]",
+    lg: "w-[42px] h-[42px]",
+  }[size];
 
   return (
     <div className={cn(stickerSize, className)} style={style}>
