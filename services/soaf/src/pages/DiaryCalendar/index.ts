4f6c486b3eb6ffe3dfcece4e771e108f29b41323
jang_yoonsu <shagrat8546@gmail.com>
2024-04-13 01:30:13 +0900

    feat : index 업데이트

--- services/soaf/src/pages/DiaryCalendar/index.ts
+++ services/soaf/src/pages/DiaryCalendar/index.ts
@@ -1,3 +1,5 @@
 import DiaryCalendar from "./DiaryCalendar";
+import DiaryDetail from "./DiaryDetail";
+import NewDiary from "./NewDiary";
 
-export { DiaryCalendar };
+export { DiaryCalendar, DiaryDetail, NewDiary };
