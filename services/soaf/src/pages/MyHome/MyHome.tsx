1a6ac9e483405f2f64a7b2d85dc505fb5ccc041c
H0onnn <dukei2017@naver.com>
2024-04-15 19:28:58 +0900

    feat : 마이 홈 수정

--- services/soaf/src/pages/MyHome/MyHome.tsx
+++ services/soaf/src/pages/MyHome/MyHome.tsx
@@ -4,9 +4,10 @@ const MyHome = () => {
   return (
     <PageLayout
       header={{
-        title: "",
-        leftSlot: null,
+        title: "마이홈",
+        leftSlot: "back",
         rightSlot: "아이콘",
+        headerClass: "bg-transparent",
       }}
     >
       <div>MyHome</div>
