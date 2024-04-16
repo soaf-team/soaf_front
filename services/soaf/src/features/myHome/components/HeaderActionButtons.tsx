0aff4de51e3ada3b29ab591425a52692f4ee5298
H0onnn <dukei2017@naver.com>
2024-04-16 17:27:49 +0900

    feat : 마이홈 header actionbutton 구현

--- services/soaf/src/features/myHome/components/HeaderActionButtons.tsx
+++ services/soaf/src/features/myHome/components/HeaderActionButtons.tsx
@@ -1,3 +1,22 @@
+import { Flex } from "@soaf/react-components-layout";
+import brush from "@/assets/icons/my-home/header/brush.svg";
+import bell from "@/assets/icons/my-home/header/bell.svg";
+import setting from "@/assets/icons/my-home/header/setting.svg";
+
+// TODO: add onClick
+
 export const HeaderActionButtons = () => {
-  return <div>actions</div>;
+  return (
+    <Flex gap={8}>
+      <div className="w-[24px] h-[24px]">
+        <img src={brush} alt="brush-icon" className="full_img_cover" />
+      </div>
+      <div className="w-[24px] h-[24px]">
+        <img src={bell} alt="brush-icon" className="full_img_cover" />
+      </div>
+      <div className="w-[24px] h-[24px]">
+        <img src={setting} alt="brush-icon" className="full_img_cover" />
+      </div>
+    </Flex>
+  );
 };
