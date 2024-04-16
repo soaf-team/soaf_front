92aab9b53dc7b5f6a17d729bccaa1c1d1eb232e4
H0onnn <dukei2017@naver.com>
2024-04-17 00:10:08 +0900

    feat : 인테리어 아이콘 추가

--- services/soaf/src/pages/MyHome/MyHome.tsx
+++ services/soaf/src/pages/MyHome/MyHome.tsx
@@ -11,31 +11,81 @@ import windowNight from "@/assets/icons/my-home/interior/window-night.svg";
 import plant from "@/assets/icons/my-home/interior/plant.svg";
 import sofa from "@/assets/icons/my-home/interior/sofa.svg";
 import movie from "@/assets/icons/my-home/interior/movie.svg";
+import up from "@/assets/icons/my-home/up.svg";
 
+import { cn } from "@/shared/utils";
 import { PageLayout } from "@shared/components";
 import { HeaderActionButtons } from "@/features/myHome/components";
+import { Flex } from "@soaf/react-components-layout";
 
 const MyHome = () => {
   const isAfter6PM = dayjs().hour() >= 18;
-  const backgroundClass = isAfter6PM ? "bg-[#0F1A2A]" : "bg-[#D3E6F4]";
+  const backgroundClass = isAfter6PM ? "bg-[#BECFDC]" : "bg-[#D3E6F4]";
 
   return (
     <PageLayout
       header={{
-        title: <span className="head6b">마이홈</span>,
-        leftSlot: "back",
         rightSlot: <HeaderActionButtons />,
         headerClass: "bg-transparent",
       }}
-      className={backgroundClass}
+      className={cn(["relative", backgroundClass])}
     >
-      <div className="fixed bottom-0 left-0 right-0 w-full max-w-[440px] h-[60%] my-0 mx-auto">
+      {/* interior img */}
+      <div className="absolute w-1/4 top-10">
+        <img src={books} alt="books" className="full_img_contain" />
+      </div>
+
+      <div className="absolute w-1/5 left-[45%]">
+        <img src={picture} alt="picture" className="full_img_contain" />
+      </div>
+
+      <div className="absolute w-1/4 top-14 right-4">
+        <img
+          src={isAfter6PM ? windowNight : windowDay}
+          alt="window"
+          className="full_img_contain"
+        />
+      </div>
+
+      <div className="absolute w-1/5 top-[15%] left-[10%]">
+        <img src={music} alt="music" className="full_img_contain" />
+      </div>
+
+      <div className="absolute w-[15%] left-1/2 top-1/4">
+        <img src={youtube} alt="youtube" className="full_img_contain" />
+      </div>
+
+      <div className="absolute z-10 w-1/4 top-1/3">
+        <img src={movie} alt="movie" className="full_img_contain" />
+      </div>
+
+      <div className="absolute z-10 top-[35%] right-1/4 w-1/10">
+        <img src={plant} alt="plant" className="full_img_contain" />
+      </div>
+
+      <div className="absolute right-[5%] top-1/3 z-10">
+        <img src={sofa} alt="sofa" className="full_img_contain" />
+      </div>
+
+      {/* bottom soaf img */}
+      <div className="absolute bottom-0 left-0 right-0 w-full max-w-[440px] h-[60%] my-0 mx-auto">
         <img
           src={isAfter6PM ? mainNight : mainDay}
           alt="main-day"
           className="full_img_cover"
         />
       </div>
+
+      {/* up button */}
+      <Flex
+        align="center"
+        justify="center"
+        className="absolute left-0 right-0 w-full bottom-10 animate-bounce"
+      >
+        <div className="w-[40px] h-[40px]">
+          <img src={up} alt="up-icon" className="full_img_cover" />
+        </div>
+      </Flex>
     </PageLayout>
   );
 };
