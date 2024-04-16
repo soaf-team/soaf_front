f326e135c13108be72d521f20b1fa353310e03e2
H0onnn <dukei2017@naver.com>
2024-04-16 17:28:07 +0900

    feat : 마이홈 페이지 (진행중)

--- services/soaf/src/pages/MyHome/MyHome.tsx
+++ services/soaf/src/pages/MyHome/MyHome.tsx
@@ -1,16 +1,41 @@
+import dayjs from "dayjs";
+
+import mainDay from "@/assets/icons/my-home/interior/main-day.svg";
+import mainNight from "@/assets/icons/my-home/interior/main-night.svg";
+import books from "@/assets/icons/my-home/interior/books.svg";
+import picture from "@/assets/icons/my-home/interior/picture.svg";
+import music from "@/assets/icons/my-home/interior/music.svg";
+import youtube from "@/assets/icons/my-home/interior/youtube.svg";
+import windowDay from "@/assets/icons/my-home/interior/window-day.svg";
+import windowNight from "@/assets/icons/my-home/interior/window-night.svg";
+import plant from "@/assets/icons/my-home/interior/plant.svg";
+import sofa from "@/assets/icons/my-home/interior/sofa.svg";
+import movie from "@/assets/icons/my-home/interior/movie.svg";
+
 import { PageLayout } from "@shared/components";
+import { HeaderActionButtons } from "@/features/myHome/components";
 
 const MyHome = () => {
+  const isAfter6PM = dayjs().hour() >= 18;
+  const backgroundClass = isAfter6PM ? "bg-[#0F1A2A]" : "bg-[#D3E6F4]";
+
   return (
     <PageLayout
       header={{
-        title: "마이홈",
+        title: <span className="head6b">마이홈</span>,
         leftSlot: "back",
-        rightSlot: "아이콘",
+        rightSlot: <HeaderActionButtons />,
         headerClass: "bg-transparent",
       }}
+      className={backgroundClass}
     >
-      <div>MyHome</div>
+      <div className="fixed bottom-0 left-0 right-0 w-full max-w-[440px] h-[60%] my-0 mx-auto">
+        <img
+          src={isAfter6PM ? mainNight : mainDay}
+          alt="main-day"
+          className="full_img_cover"
+        />
+      </div>
     </PageLayout>
   );
 };
