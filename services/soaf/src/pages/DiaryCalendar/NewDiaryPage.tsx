f7d66589bf690cc95b839a2b9730c1d50b182a7a
jang_yoonsu <shagrat8546@gmail.com>
2024-04-19 15:51:52 +0900

    fix : 일기 작성 폼 수정

--- services/soaf/src/pages/DiaryCalendar/NewDiaryPage.tsx
+++ services/soaf/src/pages/DiaryCalendar/NewDiaryPage.tsx
@@ -1,37 +1,18 @@
 /* eslint-disable react/prop-types */
 import { ActivityComponentType } from "@stackflow/react";
 
-import { PageLayout } from "@/shared/components";
-import x from "@assets/icons/header/x.svg";
-import { useFlow } from "../stackflow";
-import { DailyRaitingWidget } from "@/features/diary/components";
+import { BackButton, PageLayout } from "@/shared/components";
 
-type NewDiaryProps = {
-  step: number;
-};
-
-const NewDiaryPage: ActivityComponentType<NewDiaryProps> = ({ params }) => {
-  const { step = 1 } = params;
-
+const NewDiaryPage: ActivityComponentType = () => {
   return (
     <PageLayout
       header={{
-        rightSlot: <RightIcon />,
+        leftSlot: <BackButton />,
       }}
     >
-      {step === 1 && <DailyRaitingWidget />}
+      <div>작성</div>
     </PageLayout>
   );
 };
 
 export default NewDiaryPage;
-
-const RightIcon = () => {
-  const { pop } = useFlow();
-
-  return (
-    <button onClick={pop}>
-      <img src={x} alt="x" className="w-[12px] h-[12px]" />
-    </button>
-  );
-};
