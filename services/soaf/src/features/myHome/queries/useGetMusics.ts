a0e7a6f7b7bbdcbb71ff89acde80c3dbcba439e2
H0onnn <dukei2017@naver.com>
2024-04-27 10:52:31 +0900

    refactor : useGetMusics query select 적용

--- services/soaf/src/features/myHome/queries/useGetMusics.ts
+++ services/soaf/src/features/myHome/queries/useGetMusics.ts
@@ -5,7 +5,7 @@ export const useGetMusics = ({ value }: { value: string }) => {
   const fetchMusics = async (value: string) => {
     try {
       const response = await axios.get(
-        `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&artist=백예린&track=${value}&format=json`,
+        `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${value}&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&format=json`,
       );
 
       return response.data;
@@ -19,6 +19,7 @@ export const useGetMusics = ({ value }: { value: string }) => {
     queryFn: () => fetchMusics(value),
     staleTime: Infinity,
     enabled: value.length > 0,
+    select: (data) => data.results.trackmatches.track,
   });
 
   return {
