103351017f0da37fb27108ce40060896a76297fe
jang_yoonsu <shagrat8546@gmail.com>
2024-04-29 23:22:03 +0900

    feat : 다이어리 반응 컴포넌트 기능 추가

--- services/soaf/src/features/diary/components/DiaryReaction.tsx
+++ services/soaf/src/features/diary/components/DiaryReaction.tsx
@@ -5,8 +5,23 @@ import heartCircle from "@assets/icons/heart-circle.svg";
 import reactionCloud from "@assets/icons/reaction-cloud.svg";
 
 import best from "@assets/icons/emoji/best.svg";
+import sympathy from "@assets/icons/emoji/sympathy.svg";
+import cheer from "@assets/icons/emoji/cheer.svg";
+import okay from "@assets/icons/emoji/okay.svg";
+import impression from "@assets/icons/emoji/impression.svg";
+import amazing from "@assets/icons/emoji/amazing.svg";
+import fighting from "@assets/icons/emoji/fighting.svg";
+import funny from "@assets/icons/emoji/funny.svg";
+import angry from "@assets/icons/emoji/angry.svg";
+import sad from "@assets/icons/emoji/sad.svg";
 
-export const DiaryReaction = () => {
+type DiaryReactionProps = {
+  reactions: {
+    [key: string]: number;
+  };
+};
+
+export const DiaryReaction = ({ reactions }: DiaryReactionProps) => {
   const [isOpened, setIsOpened] = useState(false);
 
   const handleHeartButtonClick = () => {
@@ -15,15 +30,33 @@ export const DiaryReaction = () => {
 
   return (
     <div className="relative w-full border-t border-solid border-border h-[85px] py-[16px]">
-      <div className="flex items-center gap-[10px] body4">
-        <img
-          src={heartCircle}
-          alt="heart-circle"
-          className="w-[20px] h-[20px]"
-          onClick={handleHeartButtonClick}
-        />
-        일기에 대한 따뜻한 마음을 남겨보세요.
-      </div>
+      {Object.keys(reactions).length > 0 ? (
+        <div className="flex flex-wrap gap-[6px]">
+          {Object.entries(reactions).map(([key, count]) => (
+            <div
+              key={key}
+              className="flex items-center gap-[4px] label4 text-black/70"
+            >
+              <img
+                src={REACTION_EMOJI[key as keyof typeof REACTION_EMOJI].icon}
+                alt={REACTION_EMOJI[key as keyof typeof REACTION_EMOJI].label}
+                className="w-[20px] h-[20px]"
+              />
+              {REACTION_EMOJI[key as keyof typeof REACTION_EMOJI].label} {count}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center gap-[10px] body4">
+          <img
+            src={heartCircle}
+            alt="heart-circle"
+            className="w-[22px] h-[20px]"
+            onClick={handleHeartButtonClick}
+          />
+          일기에 대한 따뜻한 마음을 남겨보세요.
+        </div>
+      )}
       {createPortal(
         <ReactionCloud
           isVisible={isOpened}
@@ -81,7 +114,7 @@ const ReactionCloud = ({
       />
       <div className="absolute flex bottom-[78px] left-[16px] m-auto w-[288px] h-[133px] z-[10000]">
         <div className="absolute w-full h-full grid grid-cols-5 gap- py-[12px] px-[12px]">
-          {REACTION_EMOJI.map((emoji, index) => (
+          {Object.values(REACTION_EMOJI).map((emoji, index) => (
             <div
               key={index}
               onClick={() => handleEmojiClick(emoji.label)}
@@ -106,15 +139,15 @@ const ReactionCloud = ({
   );
 };
 
-const REACTION_EMOJI = [
-  { label: "최고예요", icon: best },
-  { label: "공감해요", icon: best },
-  { label: "응원해요", icon: best },
-  { label: "괜찮아요", icon: best },
-  { label: "감동이예요", icon: best },
-  { label: "대단해요", icon: best },
-  { label: "힘내요", icon: best },
-  { label: "웃겨요", icon: best },
-  { label: "화냐요", icon: best },
-  { label: "슬퍼요", icon: best },
-];
+const REACTION_EMOJI = {
+  best: { label: "최고예요", icon: best },
+  sympathy: { label: "공감해요", icon: sympathy },
+  cheer: { label: "응원해요", icon: cheer },
+  okay: { label: "괜찮아요", icon: okay },
+  impression: { label: "감동이예요", icon: impression },
+  amazing: { label: "대단해요", icon: amazing },
+  fighting: { label: "힘내요", icon: fighting },
+  funny: { label: "웃겨요", icon: funny },
+  angry: { label: "화냐요", icon: angry },
+  sad: { label: "슬퍼요", icon: sad },
+};
