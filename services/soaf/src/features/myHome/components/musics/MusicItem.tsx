acac57bbae51e89b9d1c473e811cacdbb046ff4c
H0onnn <dukei2017@naver.com>
2024-05-01 19:10:58 +0900

    fix : props 오타 수정

--- services/soaf/src/features/myHome/components/musics/MusicItem.tsx
+++ services/soaf/src/features/myHome/components/musics/MusicItem.tsx
@@ -52,7 +52,7 @@ export const MusicItem = ({
       </div>
 
       <Flex
-        direction="comlumn"
+        direction="column"
         justify={cn({
           "space-between": type === "list",
         })}
