a0096de4762d104e5354c2724eee1e0242ab650e
H0onnn <dukei2017@naver.com>
2024-05-06 14:23:20 +0900

    feat : movie interface

--- services/soaf/src/shared/types/movie.d.ts
+++ services/soaf/src/shared/types/movie.d.ts
@@ -21,3 +21,38 @@ export interface Movie {
   vote_average?: number;
   vote_count?: number;
 }
+
+export interface Credits {
+  id: number;
+  cast: Cast[];
+  crew: Crew[];
+}
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface Crew {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  credit_id: string;
+  department: string; // Directing의 경우 감독
+  job: string;
+}
