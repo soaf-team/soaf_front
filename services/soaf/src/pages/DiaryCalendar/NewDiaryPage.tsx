f945562974a2aabc339aa3bc817a368573e30e1c
jang_yoonsu <shagrat8546@gmail.com>
2024-05-08 23:05:05 +0900

    feat : 다이어리 작성 페이지 취소 다이얼로그 추가

--- services/soaf/src/pages/DiaryCalendar/NewDiaryPage.tsx
+++ services/soaf/src/pages/DiaryCalendar/NewDiaryPage.tsx
@@ -5,8 +5,22 @@ import { ActivityComponentType } from "@stackflow/react";
 import { useFlow } from "../stackflow";
 import { useDiaryStore } from "@/features/diary/store";
 
-import { BackButton, PageLayout, XButton } from "@/shared/components";
+import {
+  BackButton,
+  Button,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  PageLayout,
+  XButton,
+} from "@/shared/components";
 import { DiaryForm } from "@/features/diary/components";
+import { Flex } from "@soaf/react-components-layout";
 
 const NewDiaryPage: ActivityComponentType = () => {
   const { pop, replace } = useFlow();
@@ -21,8 +35,9 @@ const NewDiaryPage: ActivityComponentType = () => {
   const isUnusualApproach =
     diary.emotions.length === 0 || diary.rating === 0 || !diary.date;
 
-  const handleXButtonClick = () => {
+  const handleCancelClick = () => {
     pop(3);
+    resetAllDiaryState();
   };
 
   useEffect(() => {
@@ -37,20 +52,47 @@ const NewDiaryPage: ActivityComponentType = () => {
   }
 
   return (
-    <PageLayout
-      header={{
-        leftSlot: <BackButton />,
-        rightSlot: <XButton onClick={handleXButtonClick} />,
-      }}
-    >
-      <DiaryForm
-        diary={diary}
-        handleReorderEmotions={onChangeEmotionOrder}
-        handleTitleChange={onChangeTitle}
-        handleContentChange={onChangeContent}
-        handlePhotosChange={onChangePhotos}
-      />
-    </PageLayout>
+    <Dialog>
+      <PageLayout
+        header={{
+          leftSlot: <BackButton />,
+          rightSlot: (
+            <DialogTrigger>
+              <XButton onClick={() => {}} />
+            </DialogTrigger>
+          ),
+        }}
+      >
+        <DiaryForm
+          diary={diary}
+          handleReorderEmotions={onChangeEmotionOrder}
+          handleTitleChange={onChangeTitle}
+          handleContentChange={onChangeContent}
+          handlePhotosChange={onChangePhotos}
+        />
+      </PageLayout>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>일기를 그만 쓸까요?</DialogTitle>
+          <DialogDescription>지금까지 입력한 내용이 사라져요</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Flex gap={8} className="w-full">
+            <DialogClose className="flex-1">
+              <Button size="sm" variant="secondary">
+                아니요
+              </Button>
+            </DialogClose>
+            <DialogClose className="flex-1">
+              <Button size="sm" onClick={handleCancelClick} variant="warn">
+                네, 취소할래요
+              </Button>
+            </DialogClose>
+          </Flex>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
