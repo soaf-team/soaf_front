8e9586f25f7013372f7165ca514a7cb39d7d7474
jang_yoonsu <shagrat8546@gmail.com>
2024-05-09 23:18:08 +0900

    feat : 다이어리 작성 페이지 하단에 키보드 위에 위치한 바 컴포넌트 추가

---
+++ services/soaf/src/features/diary/components/AboveKeyboardBar.tsx
@@ -0,0 +1,50 @@
+import { DiaryFormType } from "../store";
+import { cn } from "@/shared/utils";
+
+import { Flex } from "@soaf/react-components-layout";
+
+import photo from "@/assets/icons/shared/photo.svg";
+import lock from "@/assets/icons/shared/lock.svg";
+import unLock from "@/assets/icons/shared/unlock.svg";
+
+type AboveKeyboardBarProps = {
+  diary: DiaryFormType;
+  handleAddPhoto: (photos: string[]) => void;
+  handleSaveDiary: () => void;
+  togglePrivate: () => void;
+};
+
+export const AboveKeyboardBar = ({
+  diary,
+  handleAddPhoto,
+  handleSaveDiary,
+  togglePrivate,
+}: AboveKeyboardBarProps) => {
+  const contentLengthColor =
+    diary.content.length > 2000 ? "text-red" : "text-gray300";
+
+  return (
+    <Flex
+      justify="space-between"
+      className="fixed bottom-0 left-0 right-0 w-full h-[39px] border-t border-solid border-gray100 px-[18px]"
+    >
+      <Flex align="center" gap={16}>
+        <img src={photo} alt="photo" onClick={() => {}} />
+        <img
+          src={diary.private ? lock : unLock}
+          alt="emoji"
+          onClick={togglePrivate}
+        />
+      </Flex>
+      <Flex align="center" gap={16}>
+        <span>
+          <span className={cn(contentLengthColor)}>{diary.content.length}</span>
+          /2000
+        </span>
+        <button className="head6sb" onClick={handleSaveDiary}>
+          저장
+        </button>
+      </Flex>
+    </Flex>
+  );
+};
