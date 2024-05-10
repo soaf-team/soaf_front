80462cf96e668cedda677820bdbfc5f8667c2ede
H0onnn <dukei2017@naver.com>
2024-05-10 17:05:48 +0900

    feat : MovieItem 구현

--- services/soaf/src/features/myHome/components/movie/MovieItem.tsx
+++ services/soaf/src/features/myHome/components/movie/MovieItem.tsx
@@ -1,28 +1,40 @@
+import { useMemo } from "react";
+
 import { Flex } from "@soaf/react-components-layout";
 import { StarRating } from "@/shared/components";
-import { Movie } from "@/shared/types";
+import { Movie, MovieDetail } from "@/shared/types";
 
 import { cn } from "@/shared/utils";
 
-interface Props extends Movie {
+interface Props {
   type?: "search" | "set" | "detail";
   onClick?: () => void;
-  director?: string;
-  genre?: string;
-  rating?: number;
+  movie: Movie | MovieDetail;
 }
 
-export const MovieItem = ({
-  type = "search",
-  onClick,
-  title,
-  release_date,
-  overview,
-  poster_path,
-  director,
-  genre,
-  // rating,
-}: Props) => {
+export const MovieItem = ({ type = "search", onClick, movie }: Props) => {
+  const isMovieDetail = (movie: Movie | MovieDetail): movie is MovieDetail => {
+    return (movie as MovieDetail).genres !== undefined;
+  };
+
+  const director = useMemo((): string => {
+    if (!isMovieDetail(movie)) return "";
+
+    return movie.credits.crew.filter(
+      (crew) => crew.department === "Directing",
+    )[0].name;
+  }, [movie]);
+
+  const genre = useMemo((): string => {
+    if (!isMovieDetail(movie)) return "";
+
+    return movie.genres.reduce((acc, cur, idx) => {
+      return idx === movie.genres.length - 1
+        ? `${acc}${cur.name}`
+        : `${acc}${cur.name}, `;
+    }, "");
+  }, [movie]);
+
   const posterClass = cn({
     "min-w-[92px] w-[92px] h-[134px] rounded-[8px]": type === "search",
     "w-[85px] h-[124px] rounded-[8px]": type === "set",
@@ -39,40 +51,45 @@ export const MovieItem = ({
 
   return (
     <Flex
-      direction="row"
+      direction="column"
       gap={16}
-      className="py-[8px] border-solid border-b border-border"
-      onClick={onClick}
+      className="border-b border-solid border-border"
     >
-      <div className={posterClass}>
-        <img
-          src={`https://image.tmdb.org/t/p/w342${poster_path}`}
-          alt="cover"
-          className={posterClass}
-        />
-      </div>
+      <Flex direction="row" gap={16} onClick={onClick}>
+        <div className={posterClass}>
+          <img
+            src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+            alt="cover"
+            className={posterClass}
+          />
+        </div>
 
-      <Flex
-        direction="column"
-        justify={type === "search" ? "space-between" : ""}
-        gap={8}
-        className="py-[8px]"
-      >
-        <Flex direction="column" gap={type === "search" ? 4 : 8}>
-          <p className={cn("line-clamp-1", titleClass)}>{title}</p>
-          <Flex direction="column" gap={4}>
-            {director && <p className="label3 text-gray400">{director}</p>}
-            <p className="label4 text-gray400">{release_date}</p>
+        <Flex
+          direction="column"
+          justify={type === "search" ? "space-between" : ""}
+          gap={8}
+          className="py-[8px]"
+        >
+          <Flex direction="column" gap={type === "search" ? 4 : 8}>
+            <p className={cn("line-clamp-1", titleClass)}>{movie.title}</p>
+            <Flex direction="column" gap={4}>
+              {isMovieDetail(movie) && (
+                <p className="label3 text-gray400">{director}</p>
+              )}
+              <p className="label4 text-gray400">{movie.release_date}</p>
+            </Flex>
           </Flex>
+          {type === "search" && (
+            <p className="line-clamp-3 body4">{movie.overview}</p>
+          )}
+          {type === "detail" && <StarRating size={24} onChange={() => {}} />}
         </Flex>
-        {type === "search" && <p className="line-clamp-3 body4">{overview}</p>}
-        {type === "detail" && <StarRating size={24} onChange={() => {}} />}
       </Flex>
 
-      {type === "detail" && (
-        <Flex direction="column" justify="space-between" className="py-[8px]">
-          <p className="label4 text-gray400">장르</p>
-          <p className="label4 text-gray400">{genre}</p>
+      {isMovieDetail(movie) && (
+        <Flex direction="row" justify="space-between" className="py-[8px]">
+          <p className="label2 text-gray500">장르</p>
+          <p className="label2 text-gray600">{genre}</p>
         </Flex>
       )}
     </Flex>
