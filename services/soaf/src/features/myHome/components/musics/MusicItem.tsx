a3c4af01ce9f04b033c188d94a3ce6bb81a9cd3f
H0onnn <dukei2017@naver.com>
2024-05-10 17:04:48 +0900

    feat : props 구조 변경 및 기본 이미지 적용

--- services/soaf/src/features/myHome/components/musics/MusicItem.tsx
+++ services/soaf/src/features/myHome/components/musics/MusicItem.tsx
@@ -1,3 +1,5 @@
+import defaultImg from "@assets/icons/my-home/music-default.svg";
+
 import { Flex } from "@soaf/react-components-layout";
 import { Album, IAlbumDetail } from "@/shared/types";
 
@@ -7,15 +9,9 @@ interface Props {
   music: Album | IAlbumDetail;
   type?: "search" | "list" | "detail";
   onClick?: () => void;
-  review?: string;
 }
 
-export const MusicItem = ({
-  type = "search",
-  onClick,
-  music,
-  review,
-}: Props) => {
+export const MusicItem = ({ type = "search", onClick, music }: Props) => {
   const coverClass = cn({
     "min-w-[56px] w-[56px] h-[56px] rounded-[4px]": type === "search",
     "w-[88px] h-[88px] rounded-[8px]": type === "list",
@@ -48,23 +44,22 @@ export const MusicItem = ({
       onClick={onClick}
     >
       <div className={coverClass}>
-        <img src={music.image[3]["#text"]} alt="cover" className={coverClass} />
+        <img
+          src={
+            music.image[3]["#text"] === ""
+              ? defaultImg
+              : music.image[3]["#text"]
+          }
+          alt="cover"
+          className={coverClass}
+        />
       </div>
 
-      <Flex
-        direction="column"
-        justify={cn({
-          "space-between": type === "list",
-        })}
-      >
-        <Flex direction="column" align="left" gap={4} className="py-[8px]">
-          <p className={cn("line-clamp-1", titleClass)}>
-            {music.name === "(null)" ? "제목 불명" : music.name}
-          </p>
-          <p className={cn("line-clamp-1", artistClass)}>{music.artist}</p>
-        </Flex>
-
-        {review && <p className="body4">{review}</p>}
+      <Flex direction="column" align="left" gap={4} className="py-[8px]">
+        <p className={cn("line-clamp-1", titleClass)}>
+          {music.name === "(null)" ? "제목 불명" : music.name}
+        </p>
+        <p className={cn("line-clamp-1", artistClass)}>{music.artist}</p>
       </Flex>
     </Flex>
   );
