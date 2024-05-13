ad675e1c0b276c3c62583b17fdb1b54039565996
H0onnn <dukei2017@naver.com>
2024-05-13 18:13:38 +0900

    feat : book data 훅 추가

--- services/soaf/src/features/myHome/queries/index.ts
+++ services/soaf/src/features/myHome/queries/index.ts
@@ -3,3 +3,5 @@ export * from "./useGetMusics";
 export * from "./useMusicDetail";
 export * from "./useGetMovies";
 export * from "./useMovieDetail";
+export * from "./useGetBooks";
+export * from "./useBookDetail";
