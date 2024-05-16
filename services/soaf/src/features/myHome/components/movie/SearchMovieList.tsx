7d6c81232902a5b9868ad9493a518f0689697418
H0onnn <dukei2017@naver.com>
2024-05-16 15:41:15 +0900

    feat : useGetMovies 훅 무한스크롤 적용

--- services/soaf/src/features/myHome/components/movie/SearchMovieList.tsx
+++ services/soaf/src/features/myHome/components/movie/SearchMovieList.tsx
@@ -4,6 +4,7 @@ import { useGetMovies } from "../../queries";
 import { Movie } from "@/shared/types";
 import { SearchInput } from "../SearchInput";
 import { MovieItem } from "./MovieItem";
+import { useObserver } from "@/shared/hooks";
 
 interface Props {
   onNextStep: () => void;
@@ -12,26 +13,32 @@ interface Props {
 
 export const SearchMovieList = ({ onNextStep, setMovieId }: Props) => {
   const [searchQuery, setSearchQuery] = useState("");
-  const { movies } = useGetMovies({ value: searchQuery });
+  const { movies, handleFetchNextPage, isFetching } = useGetMovies({
+    value: searchQuery,
+  });
+  const pageRef = useObserver(() => handleFetchNextPage());
 
   const handleItemClick = (movie: Movie) => {
     setMovieId(movie.id.toString());
     onNextStep();
   };
 
-  if (!movies) return null;
-
   return (
     <>
       <SearchInput type="movie" setSearchQuery={setSearchQuery} />
 
-      {movies.map((movie: Movie) => (
+      {movies?.map((movie: Movie) => (
         <MovieItem
           key={movie.id}
           movie={movie}
           onClick={() => handleItemClick(movie)}
         />
       ))}
+      {isFetching ? (
+        <div>로딩 중...</div>
+      ) : (
+        <div ref={pageRef} className="h-px" />
+      )}
     </>
   );
 };
