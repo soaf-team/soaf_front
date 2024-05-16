51bc201d8cd993d5437793b8ed769c6da3bbbc9a
H0onnn <dukei2017@naver.com>
2024-05-16 14:54:20 +0900

    feat : useGetBooks 훅 무한스크롤 적용

--- services/soaf/src/features/myHome/components/books/SearchBookList.tsx
+++ services/soaf/src/features/myHome/components/books/SearchBookList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useGetBooks } from "../../queries";
+import { useObserver } from "@/shared/hooks";
 
 import { Document } from "@/shared/types";
 import { SearchInput } from "../SearchInput";
@@ -12,7 +13,10 @@ interface Props {
 
 export const SearchBookList = ({ onNextStep, setBookId }: Props) => {
   const [searchQuery, setSearchQuery] = useState("");
-  const { books } = useGetBooks({ value: searchQuery });
+  const { books, handleFetchNextPage, isFetching } = useGetBooks({
+    value: searchQuery,
+  });
+  const pageRef = useObserver(() => handleFetchNextPage());
 
   const handleItemClick = (book: Document) => {
     setBookId(book.isbn);
@@ -30,6 +34,11 @@ export const SearchBookList = ({ onNextStep, setBookId }: Props) => {
           onClick={() => handleItemClick(book)}
         />
       ))}
+      {isFetching ? (
+        <div>로딩 중...</div>
+      ) : (
+        <div ref={pageRef} className="h-px" />
+      )}
     </>
   );
 };
