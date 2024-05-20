1ce1ac9d07a5380901e1d38baf3b0df7fb90e653
jang_yoonsu <shagrat8546@gmail.com>
2024-05-20 23:20:37 +0900

    feat : 다이어리 작성 취소 모달 추가

--- services/soaf/src/pages/DiaryCalendar/NewDiaryStep1.tsx
+++ services/soaf/src/pages/DiaryCalendar/NewDiaryStep1.tsx
@@ -5,19 +5,38 @@ import { useFlow } from "../stackflow";
 import { useDiaryStore } from "@/features/diary/store";
 
 import { Flex } from "@soaf/react-components-layout";
-import { DailyRaitingWidget } from "@/features/diary/components";
-import { PageLayout, Spacing, XButton } from "@/shared/components";
+import {
+  Dialog,
+  DialogTrigger,
+  PageLayout,
+  Spacing,
+  XButton,
+} from "@/shared/components";
+import {
+  DailyRaitingWidget,
+  DiaryWriteCancelDialog,
+} from "@/features/diary/components";
+
+const 임시닉네임 = "뽀송하루";
 
 const NewDiaryStep1: ActivityComponentType = () => {
   const { diary, onChangeRating, resetAllDiaryState, onChangeDate } =
     useDiaryStore();
   const { push, pop } = useFlow();
-  const nickname = "뽀송하루";
 
-  const handleXButtonClick = () => {
+  const canBackWithoutDialog = diary.rating == null;
+  const headerRightButton = canBackWithoutDialog ? (
+    <XButton onClick={handleXButtonClick} />
+  ) : (
+    <DialogTrigger>
+      <XButton onClick={() => {}} />
+    </DialogTrigger>
+  );
+
+  function handleXButtonClick() {
     pop(1);
     resetAllDiaryState();
-  };
+  }
 
   const handleSelectRating = (rating: number) => {
     onChangeRating(rating);
@@ -29,22 +48,21 @@ const NewDiaryStep1: ActivityComponentType = () => {
   }, []);
 
   return (
-    <PageLayout
-      header={{
-        rightSlot: <XButton onClick={handleXButtonClick} />,
-      }}
-    >
-      <p className="body2 text-gray300 text-center mb-[6px]">STEP 1/2</p>
-      <Flex direction="column" align="center" className="text-center">
-        <h2 className="head3">{nickname}님,</h2>
-        <h2 className="head3">오늘 하루는 어떠셨나요?</h2>
-        <Spacing size={24} />
-        <DailyRaitingWidget
-          selectedRating={diary.rating}
-          handleSelectRating={handleSelectRating}
-        />
-      </Flex>
-    </PageLayout>
+    <Dialog>
+      <PageLayout header={{ rightSlot: headerRightButton }}>
+        <p className="body2 text-gray300 text-center mb-[6px]">STEP 1/2</p>
+        <Flex direction="column" align="center" className="text-center">
+          <h2 className="head3">{임시닉네임}님,</h2>
+          <h2 className="head3">오늘 하루는 어떠셨나요?</h2>
+          <Spacing size={24} />
+          <DailyRaitingWidget
+            selectedRating={diary.rating}
+            handleSelectRating={handleSelectRating}
+          />
+        </Flex>
+      </PageLayout>
+      <DiaryWriteCancelDialog />
+    </Dialog>
   );
 };
 
