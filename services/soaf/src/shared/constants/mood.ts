47ea88f50bf0614675892a70486b8ec1c11b1fe0
jang_yoonsu <shagrat8546@gmail.com>
2024-06-01 20:50:16 +0900

    feat : 일기 기분 관련 상수 분리

---
+++ services/soaf/src/shared/constants/mood.ts
@@ -0,0 +1,15 @@
+import level1 from "@assets/emotions/5level/level1.svg";
+import level2 from "@assets/emotions/5level/level2.svg";
+import level3 from "@assets/emotions/5level/level3.svg";
+import level4 from "@assets/emotions/5level/level4.svg";
+import level5 from "@assets/emotions/5level/level5.svg";
+
+export const LEVELS = [level1, level2, level3, level4, level5];
+
+export const LEVEL_COLOR = [
+  "#848484",
+  "#D2D9D9",
+  "#BBE3EF",
+  "#6FD8F5",
+  "#48A4F8",
+];
