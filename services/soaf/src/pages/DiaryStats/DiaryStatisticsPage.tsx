49f4b4df7e77ca47832410aaadaac76fc8cfe278
jang_yoonsu <shagrat8546@gmail.com>
2024-06-08 10:35:05 +0900

    design : add padding bottom 20px

--- services/soaf/src/pages/DiaryStats/DiaryStatisticsPage.tsx
+++ services/soaf/src/pages/DiaryStats/DiaryStatisticsPage.tsx
@@ -1,7 +1,7 @@
 import { ActivityComponentType } from "@stackflow/react";
 import { useState } from "react";
-
 import { Flex } from "@soaf/react-components-layout";
+
 import { PageLayout, YearMonthSelect } from "@shared/components";
 import { DiaryStats } from "@/features/diary";
 
@@ -10,7 +10,7 @@ const DiaryStatsPage: ActivityComponentType = () => {
 
   return (
     <PageLayout>
-      <Flex direction="column" align="center" gap={16}>
+      <Flex direction="column" align="center" gap={16} className="pb-[20px]">
         <Flex justify="center" align="center" className="pt-[14px] pb-[6px]">
           <YearMonthSelect
             currentDate={currentDate}
