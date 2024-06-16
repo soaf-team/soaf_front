54b8bc936f34d72f94b3a2d6061969f65bf90aec
jang_yoonsu <shagrat8546@gmail.com>
2024-06-16 21:04:17 +0900

    chore : Refactor VitePWA configuration in vite.config.ts

--- services/soaf/vite.config.ts
+++ services/soaf/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,27 +9,27 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "소프",
-        short_name: "소프",
-        description: "감정일기로 찾는 소울프렌드",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        icons: [
-          {
-            src: "/logo.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/logo.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   manifest: {
+    //     name: "소프",
+    //     short_name: "소프",
+    //     description: "감정일기로 찾는 소울프렌드",
+    //     theme_color: "#ffffff",
+    //     background_color: "#ffffff",
+    //     icons: [
+    //       {
+    //         src: "/logo.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/logo.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 });
