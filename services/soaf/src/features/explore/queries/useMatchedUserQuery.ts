69b0aa598dd35bb6f79fd513996bffa324ed60bb
H0onnn <dukei2017@naver.com>
2024-06-16 13:03:45 +0900

    refactor : remove msw

--- services/soaf/src/features/explore/queries/useMatchedUserQuery.ts
+++ services/soaf/src/features/explore/queries/useMatchedUserQuery.ts
@@ -2,12 +2,64 @@ import { useQuery } from "@tanstack/react-query";
 
 import { User } from "@/shared/types";
 
-import { axiosBase } from "@/shared/apis";
+// import { axiosBase } from "@/shared/apis";
 
 export const useMatchedUserQuery = () => {
   const fetchUser = async () => {
-    const response = await axiosBase.get("/soaf-explore");
-    return response.data;
+    // const response = await axiosBase.get("/soaf-explore");
+    // return response.data;
+    return [
+      {
+        id: 1,
+        name: "나현님",
+        percent: 95,
+      },
+      {
+        id: 2,
+        name: "농담곰",
+        percent: 85,
+      },
+      {
+        id: 3,
+        name: "정훈",
+        percent: 75,
+      },
+      {
+        id: 4,
+        name: "바보유빈",
+        percent: 65,
+      },
+      {
+        id: 5,
+        name: "엉동균",
+        percent: 55,
+      },
+      {
+        id: 6,
+        name: "수현",
+        percent: 45,
+      },
+      {
+        id: 7,
+        name: "어진맴",
+        percent: 35,
+      },
+      {
+        id: 8,
+        name: "김민수",
+        percent: 25,
+      },
+      {
+        id: 9,
+        name: "최민수",
+        percent: 15,
+      },
+      {
+        id: 10,
+        name: "조민수",
+        percent: 5,
+      },
+    ] as User[];
   };
 
   const { data: matchedUsers = [] as User[] } = useQuery({
--- services/soaf/src/features/myHome/queries/useInteriorItems.ts
+++ services/soaf/src/features/myHome/queries/useInteriorItems.ts
@@ -2,15 +2,90 @@ import { useQuery } from "@tanstack/react-query";
 
 import { Interior } from "@/shared/types";
 
-import { axiosBase } from "@/shared/apis";
+// import { axiosBase } from "@/shared/apis";
 
 // TODO: 각 유저마다 인테리어 아이템이나 위치가 다를 것이기 때문에
 // 유저 id를 params로 받아와서 해당 유저의 인테리어 아이템을 가져오도록 수정해야 함
 
 export const useInteriorItems = () => {
   const fetchItems = async (): Promise<Interior[]> => {
-    const response = await axiosBase.get("/interior-items");
-    return response.data;
+    // const response = await axiosBase.get("/interior-items");
+    // return response.data;
+
+    return [
+      {
+        id: 1,
+        name: "books",
+        src: "@/assets/icons/my-home/interior/books.svg",
+        type: "hobby",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 2,
+        name: "movie",
+        src: "../assets/icons/my-home/interior/movie.svg",
+        type: "hobby",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 3,
+        name: "music",
+        src: "../assets/icons/my-home/interior/music.svg",
+        type: "hobby",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 4,
+        name: "picture",
+        src: "../assets/icons/my-home/interior/picture.svg",
+        type: "interior",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 5,
+        name: "plant",
+        src: "../assets/icons/my-home/interior/plant.svg",
+        type: "interior",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 6,
+        name: "sofa",
+        src: "../assets/icons/my-home/interior/sofa.svg",
+        type: "interior",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 7,
+        name: "windowDay",
+        src: "../assets/icons/my-home/interior/window-day.svg",
+        type: "interior",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 8,
+        name: "windowNight",
+        src: "../assets/icons/my-home/interior/window-night.svg",
+        type: "interior",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+      {
+        id: 9,
+        name: "youtube",
+        src: "../assets/icons/my-home/interior/youtube.svg",
+        type: "hobby",
+        position: { x: 0, y: 0 },
+        isVisible: true,
+      },
+    ] as Interior[];
   };
 
   const { data: interiorItems = [] as Interior[] } = useQuery<Interior[]>({
